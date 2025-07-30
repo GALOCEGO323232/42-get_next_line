f9b97102543d3fa4ef36fc1c1e8d9dcae615e70e
GALO CEGO <dragaomatadordeporco@gmail.com>
2025-07-30 19:23:18 -0300

    Add files via upload

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgagliar <kgagliar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 19:54:17 by kgagliar          #+#    #+#             */
+/*   Updated: 2025/07/30 19:08:52 by kgagliar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_gnl_strchr(const char *s, int c);
+char	*ft_gnl_strdup(const char *s);
+char	*ft_gnl_strjoin(char *s1, char *s2);
+size_t	ft_gnl_strlen(const char *s);
+
+#endif
